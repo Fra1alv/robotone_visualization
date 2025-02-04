6d5a612ace62e744631fe4542ebbee4a66244c71
Fra1alv <alonsofra1alv@gmail.com>
2025-02-04 03:06:35 -0500

    fix launch files

---
+++ launch/robotone_visualization_launch.py
@@ -0,0 +1,79 @@
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch.conditions import IfCondition
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    urdf_path = PathJoinSubstitution([
+        FindPackageShare("robotone_description"), 
+        "urdf", 
+        "robotone.urdf.xacro"
+    ])
+
+    ld = LaunchDescription([
+         DeclareLaunchArgument(
+             name='urdf',
+             default_value=urdf_path,
+             description='URDF path'
+         ),
+         DeclareLaunchArgument(
+             name='publish_joints',
+             default_value='true',
+             description='Launch joint_states_publisher'
+         ),
+         DeclareLaunchArgument(
+             name='use_sim_time', 
+             default_value='false',
+             description='Use simulation time'
+         ),
+         DeclareLaunchArgument(
+             name='rviz', 
+             default_value='true',
+             description='Run rviz'
+         ),
+    ])
+        
+    joint_state_Node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        condition=IfCondition(LaunchConfiguration("publish_joints")),
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}, {"publish_rate": 5.0}]
+    )
+ 
+    robot_state_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        parameters=[{
+            'use_sim_time': LaunchConfiguration('use_sim_time'),
+            'robot_description': Command([ 'xacro ', LaunchConfiguration('urdf')])
+        }]
+    )
+ 
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        condition=IfCondition(LaunchConfiguration("rviz")),
+        arguments=[
+            '-d',
+            os.path.join(
+                get_package_share_directory('robotone_visualization'),
+                'rviz',
+                'robotone_config.rviz'  # Make sure this file exists
+            ),
+        ],
+        parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
+    )
+
+    ld.add_action(joint_state_Node)
+    ld.add_action(robot_state_node)
+    ld.add_action(rviz_node)
+
+    return ld
